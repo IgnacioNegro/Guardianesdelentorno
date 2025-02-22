8cb1ed620ff069fbc4d9fe2876c22c0b9319b07e
Fernando <fernandobonilla832@gmail.com>
2025-02-21 21:24:12 -0300

    - Especie Modal  - Ahora el especie modal recibe objeto areaEncontrada, asi podemos mostrar a que area esta asociada esa especie

--- src/components/ListaEspecies/ListaEspecies.js
+++ src/components/ListaEspecies/ListaEspecies.js
@@ -3,12 +3,21 @@ import './ListaEspecies.css';
 import Especie from '../Especie/Especie';
 
 const ListaEspecies = ({arrayEspecies, arrayAreasNaturales}) => {    
+    console.log("Lista Especies: ", arrayAreasNaturales)
 
     return (
         <div className='areas-especies-container container mb-5'>
             <h1 className='text-center mt-5'>Lista Especies</h1>
-            {                
-                arrayEspecies.map(especie => <Especie key={especie.id} especie={especie}/>)
+            {  
+   
+                arrayEspecies.map(especie => {
+                    const areaEncontrada = arrayAreasNaturales.find(area => area.id == especie.naturalAreaId)
+                    console.log("Lista Especies: area encontrada ", areaEncontrada)
+
+                    return (
+                        <Especie key={especie.id} especie={especie} area={areaEncontrada}/>
+                    )
+                })               
             }
         </div>
     )
