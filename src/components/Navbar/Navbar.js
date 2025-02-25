7d7036a0581c9360143c2cd78d8c03f33d8ab0ce
Fernando <fernandobonilla832@gmail.com>
2025-02-25 15:13:29 -0300

    - Navbar  - Removido el container-fluid por solo container asi se los elementos se ven un poco mas centrados

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
         <>
             <div id='Navbar-container'>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
+                    <div className="container">
                         <a className="navbar-brand" href="#">Navbar</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
