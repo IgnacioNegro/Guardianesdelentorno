9d9e6caa59c0d2ba7ade1cfe71b27672ff0af4d2
IgnacioNegro <negroignacio2014@gmail.com>
2025-02-25 16:53:05 -0300

    Agregado filtro para especie y áreas. Editado Lista Areas y Lista especies. Ignacio 26/2

--- src/components/ListaAreasNaturales/ListaAreas.js
+++ src/components/ListaAreasNaturales/ListaAreas.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ListaAreas.css';
 import AreaNatural from '../AreaNatural/AreaNatural';
 import BotonCrearArea from '../Crear Area/Boton Crear Area.js';
 import { useSelector } from 'react-redux';
 
+
 const ListaAreas = ({arrayAreasNaturales, arrayEspecies}) => {    
     const user = useSelector((state) => state.user);
     //console.log("especiesAvistadas en lista areas: ", arrayEspecies);
 
+    const [BarraDeBusqueda, setBarraDeBusqueda] = useState("");
+
+    const areasFiltradas = arrayAreasNaturales.filter(area =>
+        area.name?.toLowerCase().includes(BarraDeBusqueda.toLowerCase()) || 
+        area.type?.toLowerCase().includes(BarraDeBusqueda.toLowerCase()) || 
+        area.region?.toLowerCase().includes(BarraDeBusqueda.toLowerCase()) || 
+        area.conservationStatus?.toLowerCase().includes(BarraDeBusqueda.toLowerCase())  
+    );
     return (
         <div className='areas-especies-container container mb-5' id='listado-areas-naturales'>
             <h1 className='text-center mt-5'>Lista Areas Naturales</h1>
             {user ? <BotonCrearArea /> : <></>}
-            
+
+            <input
+                type="text"
+                placeholder="Buscar área"
+                className="form-control my-3"
+                value={BarraDeBusqueda}
+                onChange={(e) => setBarraDeBusqueda(e.target.value)}
+            />
+
+        {areasFiltradas.length > 0 ? (
+                areasFiltradas.map(area => {
+                    let especiesAvistadas = arrayEspecies.filter(especie => especie.naturalAreaId === area.id);
+                    return <AreaNatural key={area.id} area={area} especiesAvistadas={especiesAvistadas} />;
+                })
+            ) : (
+                <p className="text-center">No se encontraron áreas que coincidan con la búsqueda.</p>
+            )}
+        
             {                
                 arrayAreasNaturales.map(area => {
                     let especiesAvistadas = [] // por cada area dentro del array va a buscar todas las especies avistadas asociadas a esa area, y devuelve un array con la/s especie/s o un array vacio.
--- src/components/ListaEspecies/ListaEspecies.js
+++ src/components/ListaEspecies/ListaEspecies.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ListaEspecies.css';
 import Especie from '../Especie/Especie';
 import BotonCrearEspecie from '../Crear Especie/BotonCrearEspecie';
@@ -8,12 +8,40 @@ const ListaEspecies = ({arrayEspecies, arrayAreasNaturales}) => {
     const user = useSelector((state) => state.user);
     console.log( "lista especie: ", Array.isArray(arrayAreasNaturales));
 
+        const [BarraDeBusqueda, setBarraDeBusqueda] = useState("");
+        
+    const especiesFiltradas = arrayEspecies.filter(especie =>
+        especie.commonName?.toLowerCase().includes(BarraDeBusqueda.toLowerCase()) || 
+        especie.scientificName?.toLowerCase().includes(BarraDeBusqueda.toLowerCase()) || 
+        especie.category?.toLowerCase().includes(BarraDeBusqueda.toLowerCase()) || 
+        especie.conservationStatus?.toLowerCase().includes(BarraDeBusqueda.toLowerCase())  ||
+        especie.conservationStatus?.toLowerCase().includes(BarraDeBusqueda.toLowerCase())  
+    );
     return (
         <div className='areas-especies-container container mb-5' id='listado-especies'>
             <h1 className='text-center mt-5'>Lista Especies</h1>
             {user ? <BotonCrearEspecie arrayAreasNaturales={arrayAreasNaturales}  /> : <></>}
+
+            <input
+                type="text"
+                placeholder="Buscar especie"
+                className="form-control my-3"
+                value={BarraDeBusqueda}
+                onChange={(e) => setBarraDeBusqueda(e.target.value)}
+            />
+
+        {especiesFiltradas.length > 0 ? (
+                especiesFiltradas.map(area => {
+                    let especiesAvistadas = arrayEspecies.filter(especie => especie.naturalAreaId === area.id);
+                    return <Especie key={Especie.id} especie={Especie} especiesAvistadas={especiesAvistadas} />;
+                })
+            ) : (
+                <p className="text-center">No se encontraron áreas que coincidan con la búsqueda.</p>
+            )}
+        
             {  
    
+                
                 arrayEspecies.map(especie => {
                     const areaEncontrada = arrayAreasNaturales.find(area => area.id == especie.naturalAreaId)
                     //console.log("Lista Especies: area encontrada ", areaEncontrada)
